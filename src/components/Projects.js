68b17fe4a967c9efd878bc48d06bb6c111919a0f
gometi <gometica@gmail.com>
2019-06-21 14:03:11 -0400

    added some css animations

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,61 +1,72 @@
 import React from 'react';
+import {titleAnimate, imageAnimate, detailsAnimate, siteButton, githubButton} from './animate';
 
 class Projects extends React.Component {
-    constructor(props){
+    constructor() {
         super()
         this.state = {
-            titleInViewPort: false
+            titleInViewPort: false,
+            projectInViewPort: false,
+            buttonsInViewPort: false
         }
-        this.titlePosition = React.createRef();
+        this.title = React.createRef();
+        this.project = React.createRef();
+        this.buttons = React.createRef();
         this.handleScroll = this.handleScroll.bind(this);
+        this.getElementPosition = this.getElementPosition.bind(this);
     }
-    handleScroll(){
-        //get element
-      const title = this.titlePosition.current;
-      //get the element's bottom position relative to the viewport
-      const titlePosition = title.getBoundingClientRect().bottom +9;
-      //change the state for 'titleInViewPort' to true if the element's postion is inside the viewport,  ie:(element's bottom postion is less than the viewport height)
-      if(titlePosition < window.innerHeight){
-          this.setState({titleInViewPort: true})
-      }
-      else {
-        this.setState({titleInViewPort: false})
-      }
-    
+    handleScroll() {
+    this.getElementPosition(this.title, "titleInViewPort");
+    this.getElementPosition(this.project, "projectInViewPort");
+    this.getElementPosition(this.buttons, "buttonsInViewPort");
     }
 
-    componentWillMount(){
-        window.addEventListener('scroll', this.handleScroll)
+    getElementPosition(el, s) {
+        //get element
+        const element = el.current;
+        //get the element's bottom position relative to the viewport
+        const midPostion = (element.getBoundingClientRect().bottom -  element.getBoundingClientRect().top) / 2;
+        const elementPosition = element.getBoundingClientRect().bottom - midPostion;
+        //get the name of the state
+        const state = s
+        //change the state to true if the element's postion is inside the viewport,  ie:(element's bottom postion is less than the viewport height)
         
+        if (elementPosition < window.innerHeight) {
+            this.setState({ [state]: true })
+        }
+        else {
+            this.setState({ [state]: false })
+        }
     }
-    componentWillUnmount(){
+
+    componentWillMount() {
+        window.addEventListener('scroll', this.handleScroll)
+
+    }
+    componentWillUnmount() {
         window.removeEventListener('scroll', this.handleScroll)
     }
     render() {
         if (this.props.project) {
             const { title, img, details, link_site, link_github } = this.props.project
-            const titleAnimate = {
-                hide: "title animated zoomOut",
-                show: "title animated zoomIn"
-            }
             return (
                 <>
-                    <h2 ref={this.titlePosition} className={this.state.titleInViewPort ? titleAnimate.show : titleAnimate.hide}>{title}</h2>
-                    <div className="row justify-content-center">
+                    <h2 ref={this.title} className={this.state.titleInViewPort ? titleAnimate.show : titleAnimate.hide}>{title}</h2>
+                    <div ref={this.project} className="row justify-content-center">
                         <div className="col-md-6 d-flex justify-content-center">
-                            <img className="project_image  img-thumbnail" src={img} alt={title} />
+                            <img className={this.state.projectInViewPort ? imageAnimate.show : imageAnimate.hide} src={img} alt={title} />
                         </div>
                         <div className="col-md-6 d-flex justify-content-center">
-                            <h3 className="project_details text-center">{details}</h3>
+                            <h3 className={this.state.projectInViewPort ? detailsAnimate.show : detailsAnimate.hide}>{details}</h3>
                         </div>
 
                     </div>
 
-                    <div className="d-flex">
-                        <a className="m-2 links" href={link_site}><h3 className="site m-2 p-2">Site</h3></a>
-                        <a className="m-2 links" href={link_github}><h3 className="github m-2 p-2">Github</h3></a>
+                    <div ref={this.buttons} className="d-flex">
+                        <a className="m-2 links" href={link_site}><h3 className={this.state.buttonsInViewPort ? siteButton.show : siteButton.hide}>Site</h3></a>
+                        <a className="m-2 links" href={link_github}><h3 className={this.state.buttonsInViewPort ? githubButton.show : githubButton.hide}>Github</h3></a>
                     </div>
-                    <br/>
+                    <br />
                 </>
             )
         }
---
+++ src/components/animate.js
@@ -0,0 +1,24 @@
+export const titleAnimate = {
+    hide: "title animated zoomOut slow",
+    show: "title animated zoomIn slow"
+}
+
+export const imageAnimate = {
+    hide: "project_image  img-thumbnail animated bounceOutDown",
+    show: "project_image  img-thumbnail animated bounceInUp"
+}
+
+export const detailsAnimate = {
+    hide: "project_details text-center animated fadeOutDown delay-1s",
+    show: "project_details text-center animated fadeInUp delay-1s"
+}
+
+export const siteButton = {
+    hide: "site m-2 p-2 animated bounceOut",
+    show: "site m-2 p-2 animated bounceIn"
+}
+
+export const githubButton = {
+    hide: "github m-2 p-2 animated bounceOut",
+    show: "github m-2 p-2 animated bounceIn delay-1s"
+}
